9008921dcd465afbcfdeb548684089a11844c944
yeonoel <yeopevroguinoel@gmail.com>
2022-03-11 18:06:12 +0000

    This program assign a random number to the variable n and print it

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,20 +15,20 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	 if (n > 0)
-	    {
-	      printf("%d, is positive\n", n);
-	    }
-	else
-	  {
-	    if (n == 0)
-	       {
-		 printf("%d, is zero\n", n);
-	       }
-	    else
-	       {
-		 printf("%d, is negative\n", n);
-	       }
-	  }
+if (n > 0)
+{
+printf("The number, is positive \n");
+}
+else
+{
+if (n == 0)
+{
+printf("The number, is zero \n");
+}
+else
+{
+printf("The number, is negative \n");
+}
+}
 	return (0);
 }
