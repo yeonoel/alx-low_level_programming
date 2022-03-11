1ab8bde7d742e9e543666c9222d47b5ee970102b
yeonoel <yeopevroguinoel@gmail.com>
2022-03-11 01:22:30 +0000

    this program prints exactly <Programming is like building a multilingual puzzle, followed by a new line.>

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char str1[] = "Programming is like building a multilingual puzzle";
-puts(str1);
+
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
