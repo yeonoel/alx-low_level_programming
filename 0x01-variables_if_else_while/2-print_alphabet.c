1651f2ce0f271333544c191c3378678d90f49c47
yeonoel <yeopevroguinoel@gmail.com>
2022-03-12 03:09:15 +0000

    This prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int i;
+  int m = 96;
+  
+  for (i = 0; i < 26; i++)
+    {
+      m = m + 1;
+      putchar(m);
+    }
+  return (0);
+}
