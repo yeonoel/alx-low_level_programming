005dddfd172a8ff105cc51f1d02bae1355de7d19
yeonoel <yeopevroguinoel@gmail.com>
2022-03-13 04:01:41 +0000

    tHIS PROGRAM prints all possible different combinations of two digits.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+int n;
+int y = 0;
+int p = 49;
+int x = ',';
+for (n = 48; n < 57; n++)
+{
+while  (p < 58)
+{
+	 putchar(n);
+	 putchar(p);
+	 if (n != 56 || p != 57)
+	   {
+	     putchar(x);
+	     putchar(' ');
+	   }
+	 p = p + 1;
+}
+ y = y + 1;
+ p = 49;
+ p = p + y;
+}
+ putchar('\n');
+	return (0);
+}
