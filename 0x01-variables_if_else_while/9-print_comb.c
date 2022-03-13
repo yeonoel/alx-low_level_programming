fc7b56a572e96f774d96598693534c6a82fbf15d
yeonoel <yeopevroguinoel@gmail.com>
2022-03-13 01:01:57 +0000

    This programm prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+int x = ',';
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+if (n != 57)
+{
+putchar(x);
+}
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
