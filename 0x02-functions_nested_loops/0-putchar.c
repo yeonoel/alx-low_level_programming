0dd72e510be8d37e8aacfd7570bdcad78adb73a8
yeonoel <yeopevroguinoel@gmail.com>
2022-03-15 13:21:30 +0000

    prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+putchar(95);
+putchar(112);
+putchar(117);
+putchar(116);
+putchar(99);
+putchar(104);
+putchar(97);
+putcahr(114);
+putchar('\n');
+return (0);
+}
