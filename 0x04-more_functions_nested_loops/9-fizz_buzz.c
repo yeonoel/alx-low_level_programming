6b396ad2ba2e2738e6d873cf721419fd8c99b149
yeonoel <yeopevroguinoel@gmail.com>
2022-03-18 14:39:09 +0000

    This program prints the numbers  from 0 to 100 and changes the multiples of 3 and 5

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int i, n, z;
+  for (i = 1; i <= 100; i++)
+    {
+      
+      n = i % 3;
+      z = i % 5;
+      if (n == 0)
+	{
+	  printf("Fizz ");
+	}
+      else if (z == 0 )
+	{
+	  printf("Buzz ");	  
+	}
+      else if (z == 0 && n == 0)
+	{
+	  printf("FizzBuzz ");
+	}
+      else
+	{
+	  printf("%d ",i);
+	}
+            
+    }
+  printf("\n");
+  return (0);
+}
