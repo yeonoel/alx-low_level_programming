4f60a504f4b67df97dd9658a2dcaab6851c5a5a8
yeonoel <yeopevroguinoel@gmail.com>
2022-03-25 00:51:26 +0000

    This function compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,25 @@
-int _strcmp ( char  *s1 ,  char  *s2 ) 
-{ 
-    while (  (  *s1  !=  '\0'  &&  *s2  !=  '\0'  )  &&  *s1  ==  *s2) 
-    { 
-        s1 ++ ; 
-        s2 ++ ; 
-    }
 
-    if (*s1  ==  *s2 ) 
-    { 
-        return  0 ;   
-    }
+/**
+ * _strcmp - compares two strings
+ * @s1: first caracteres to compare
+ * @s2: the second caracteres to compare
+ * Return: Always 0.
+ */
 
-    else 
-    { 
-        return  *s1  -  *s2 ; 
-    } 
+int _strcmp(char *s1, char *s2)
+{
+while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+if (*s1 == *s2)
+{
+return (0);
+}
+
+else
+{
+return (*s1 - *s2);
+}
 }
