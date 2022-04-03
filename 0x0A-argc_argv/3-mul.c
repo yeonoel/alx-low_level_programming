3cba6327004b44aba984daf99b462f8122bf81ff
yeonoel <yeopevroguinoel@gmail.com>
2022-04-03 13:33:50 +0000

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: the int number of the main arguments
+ * @argv: the char of pointer table <of strings
+ * Return: Always 0 Success
+ */
+int main(int argc, char *argv[])
+{
+int x, i, result;
+result = 1;
+if (argc < 3)
+{
+return (0);
+}
+else
+{
+for (i = 1; i < argc; i++)
+{
+x = strtol(argv[i], NULL, 10);
+result = result *x;
+}
+printf("%d\n", result);
+}
+return (0);
+}
