fcba33d128079e58c01e89e908528c6158ded7c9
yeonoel <yeopevroguinoel@gmail.com>
2022-07-01 23:39:37 +0000

    This function  creates a hash table.

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: The size of the table
+ * Return: The pointer to table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int i;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(size * sizeof(hash_node_t));
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+
+
+
+}
---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,43 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* data structure */
+
+/**
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unisue in the HashTables
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each celle of this array is a pointer to the first  node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ */
+typedef struct hash_table_s
+{
+	unsigned int size;
+	hash_node_t **array;
+} hash_table_t;
+
+/* prototype of all functions */
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif /* H */
