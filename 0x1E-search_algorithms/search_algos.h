109e1214127dce575c2b6fe43c73695af50bc83b
yeonoel <yeopevroguinoel@gmail.com>
2022-10-04 14:53:35 +0000

    Linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+#include <stdio.h>
+#include <stdlib.h>
+/* prototypes function */
+int linear_search(int *array, size_t size, int value);
+
+#endif
