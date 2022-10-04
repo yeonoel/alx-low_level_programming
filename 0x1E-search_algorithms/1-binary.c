2e9fafa28fe27f435769cad03127e8fad977d245
yeonoel <yeopevroguinoel@gmail.com>
2022-10-04 16:56:54 +0000

    Binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,40 @@
+#include <math.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in array
+ * @array: the pointer to the first element of the array
+ * @size: the number of elements in array
+ * @value: the value to search
+ * Return: The first index where value is located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t L, R, mid, i;
+
+	if (array == NULL)
+		return (-1);
+	R = size - 1;
+	L = 0;
+	while (L < R)
+	{
+		mid = floor((L + R) / 2);
+		printf("Searching in array: ");
+		size = R + 1;
+		for (i = L; i < size; i++)
+		{
+			printf("%lu", i);
+			if (i < size - 1)
+				printf(", ");
+		}
+		printf("\n");
+		if (array[mid] > value)
+			R = mid - 1;
+		else if (array[mid] < value)
+			L = mid + 1;
+		else
+			return (mid);
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,7 +2,9 @@
 #define _SEARCH_ALGOS_H
 #include <stdio.h>
 #include <stdlib.h>
+
 /* prototypes function */
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
