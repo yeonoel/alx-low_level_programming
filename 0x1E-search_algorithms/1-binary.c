58beda7d22cb952cd89fbebef828bdc464c82b3c
yeonoel <yeopevroguinoel@gmail.com>
2022-10-04 23:03:38 +0000

    Binary tree

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -21,8 +21,7 @@ int binary_search(int *array, size_t size, int value)
 	{
 		mid = floor((L + R) / 2);
 		printf("Searching in array: ");
-		size = R + 1;
-		for (i = L; i < size; i++)
+		for (i = L; i < R + 1; i++)
 		{
 			printf("%d", array[i]);
 			if (i < size - 1)
